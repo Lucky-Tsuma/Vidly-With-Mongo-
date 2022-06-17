bc0e2384c7dc380ac5fdba608a80fdce517165b9
Lucky-Tsuma <tsumahmumba@gmail.com>
2022-06-17 16:31:27 +0300

    Return http status codes. Connect to db from index file

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require("express")
 const app = express()
 const genreRoutes = require("./routes/genres")
+const mongose = require("mongoose")
 require("dotenv").config()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use("/vidly.com/api", genreRoutes)
+
+mongose.connect("mongodb://localhost/vidly")
+    .then(() => console.log("Connected to database..."))
+    .catch(err => console.error("Error connecting to database: ", err))
 
 const port = process.env.VIDLY_PORT || 3000 
 
--- routes/genres.js
+++ routes/genres.js
@@ -3,10 +3,6 @@ const router = express.Router()
 const Joi = require("joi")
 const mongose = require("mongoose")
 
-mongose.connect("mongodb://localhost/vidly")
-    .then(() => console.log("Connected to database..."))
-    .catch(err => console.error("Error connecting to database: ", err))
-
     // INPUT VALIDATION WITH JOI
 const schema = Joi.object({
     name: Joi.string().min(5).required()
@@ -17,92 +13,66 @@ const validateGenre = (genre) => {
 }
 
 // MONGO_DB
-// Create schema. Template for out document
-const genreSchema = mongose.Schema({
+
+// We created a schema and compiled it into a model at the same time to make clean code
+const Genre = mongose.model("Genre", mongose.Schema({
     name: {
         type: String,
         required: true,
         min: 5,
         max: 50
     }
-})
-
-// Compile schema into model (which is similar to a class)
-const Genre = mongose.model("Genre", genreSchema)
-
-async function createGenre (gnr) {
-    try {
-        const genre = new Genre ({
-            name: gnr
-        })
-    
-        return await genre.save()
-    } catch (error) {
-        return "Error saving genre" + error
-    }
-}
-
-async function getGenres() {
-    try {
-        return await Genre.find()  
-    } catch (error) {
-        return "Error getting genres" + error
-    }
-    
-}
-
-async function updateGenre(id, name) {
-    try {
-        const genre = await Genre.findById(id)
-
-        if(!genre) { return "Sorry! No such genre was found"}
-
-        genre.name = name
-        return await genre.save()
-    } catch (error) {
-        return "Error updating genre " + error
-    }
-}
-
-async function removeGenre(id) {
-    try {
-        const genre = await Genre.findById(id)
-
-        if(!genre) { return "Sorry! No such genre was found"}
-        return await  Genre.findByIdAndRemove(id)
-    } catch (error) {
-        return "Error deleting genre " + error
-    }
-}
+}))
 
 router.get("/", (_req, res) => {
     res.send("You are on vidly homepage.")
 })
 
 router.get("/genres", async (_req, res) => { 
-    const genres = await getGenres()
-    res.send(genres)
+    try {
+        res.status(200).send(await Genre.find())  
+    } catch (error) {
+        res.status(500).send("Error getting genres" + error)
+    }
 })
 
 router.post("/genres", async (req, res) => { 
     const { error } = validateGenre(req.body)
     if (error) { return res.status(400).send(error.details[0].message) }
-    const response = await createGenre(req.body.name)
-    res.send(response)
+    
+    try {
+        const genre = new Genre ({ name: req.body.name })
+        res.status(200).send(await genre.save())
+    } catch (err) {
+        res.status(500).send("Error saving genre" + err) 
+    }
 })
 
 router.put("/genres/:id", async (req, res) => { 
     const { error } = validateGenre(req.body)
     if (error) { return res.status(400).send(error.details[0].message) }
 
-    const result = await updateGenre(req.params.id, req.body.name)
-    res.send(result)  
+    try {
+        const genre = await Genre.findById(req.params.id)
+
+        if(!genre) { return res.status(200).send("Sorry! No such genre was found")}
+
+        genre.name = req.body.name
+        res.status(200).send(await genre.save())
+    } catch (err) {
+        res.status(500).send("Error updating genre " + err)
+    }
 })
 
 router.delete("/genres/:id", async (req, res) => { 
+    try {
+        const genre = await Genre.findById(req.params.id)
 
-    const result = await removeGenre(req.params.id)
-    res.send(result)
+        if(!genre) { return res.status(200).send("Sorry! No such genre was found")}
+        res.status(200).send(await  Genre.findByIdAndRemove(req.params.id))
+    } catch (error) {
+        res.status(500).send("Error deleting genre " + error)
+    }
 })
 
 module.exports = router
